61c4899e1aeafcd686a44e9d1fdf36217f557b45
Amaan Jambura <amaanjambura786@gmail.com>
2021-06-14 18:18:31 +0530

    Created resourceful routes and controller for categories section

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CategoriesController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,3 +23,6 @@ Route::get('/dashboard', function () {
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+
+//Application routes
+Route::resource('categories', CategoriesController::class);
