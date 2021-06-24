ead6d77e03846406bae83966965c0ea04623c354
Amaan Jambura <amaanjambura786@gmail.com>
2021-06-24 22:57:36 +0530

    completed crud operations for tags

---
+++ app/Http/Controllers/TagsController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\tags\CreateTagRequest;
+use App\Http\Requests\tags\UpdateTagRequest;
+use App\Models\Tag;
+use Illuminate\Http\Request;
+
+class TagsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $tags = Tag::paginate(10);
+        return view('tags.index', compact('tags'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('tags.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(CreateTagRequest $request)
+    {
+        Tag::create([
+            'name' => "$request->name"
+        ]);
+        session()->flash('success', 'Tag created Successfully!');
+        //return to index
+        return redirect(route('tags.index'));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Tag  $tag
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Tag $tag)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Tag  $tag
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Tag $tag)
+    {
+        return view('tags.edit', compact(['tag']));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Tag  $tag
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateTagRequest $request, Tag $tag)
+    {
+        $tag->name = $request->name;
+        // $category->update(['name'=>$request->name])
+        $tag->save();
+        session()->flash('success', 'Tag Updated successfully!');
+        return redirect(route('tags.index'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Tag  $tag
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Tag $tag)
+    {
+        $tag->delete();
+        session()->flash('success', 'Tag deleted successfully!');
+        return redirect(route('tags.index'));
+    }
+}
