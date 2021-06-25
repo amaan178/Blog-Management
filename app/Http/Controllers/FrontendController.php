124d349f064ffea293c3ea8e903b630059325503
Amaan Jambura <amaanjambura786@gmail.com>
2021-06-25 21:19:44 +0530

    Created FrontendController to access tables in frontend design

---
+++ app/Http/Controllers/FrontendController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\Tag;
+use Illuminate\Http\Request;
+
+class FrontendController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::simplePaginate(3);
+        $tags = Tag::all();
+        $categories = Category::all();
+        return view('blogs.index', compact(['posts', 'tags', 'categories']));
+    }
+}
