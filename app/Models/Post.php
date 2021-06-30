aa38a3f99037f63992afee141116b23f45414ca2
Amaan Jambura <amaanjambura786@gmail.com>
2021-06-30 22:57:49 +0530

    added gaurded in Post model and other functions in Category and Post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,10 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Post extends Model
 {
     use HasFactory;
+    protected $guarded = ['id'];
+
+    public function getImagePathAttribute()
+    {
+        return 'storage/'.$this->image;
+    }
 
     public function category()
     {
@@ -22,5 +29,15 @@ class Post extends Model
     public function tags()
     {
         return $this->belongsToMany(Tag::class)->withTimestamps();
+    }
+
+    public function hasTag(int $tag_id): bool
+    {
+        return in_array($tag_id, $this->tags->pluck('id')->toArray());
+    }
+
+    public function deleteImage()
+    {
+        Storage::delete($this->image);
     }
 }
