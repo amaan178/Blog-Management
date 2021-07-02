ed78a6d1410677f78625458114bd41665e3bd1fc
Amaan Jambura <amaanjambura786@gmail.com>
2021-07-02 21:54:17 +0530

    Added middleware in kernel to use them in Routes

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,6 +2,9 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\ValidateAuthorForEditAndDeletePost;
+use App\Http\Middleware\VerifyAdmin;
+use App\Http\Middleware\VerifyCategoriesCount;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -62,5 +65,8 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'admin' => VerifyAdmin::class,
+        'validateAuthor' => ValidateAuthorForEditAndDeletePost::class,
+        'verifyCategoriesCount' => VerifyCategoriesCount::class,
     ];
 }
