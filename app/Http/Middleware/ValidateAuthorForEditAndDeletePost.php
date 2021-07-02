23d6bb24efc9e27d928e92065954abc332a7ca17
Amaan Jambura <amaanjambura786@gmail.com>
2021-07-02 21:10:33 +0530

    Created Middleware to validate author to edit and delete post

---
+++ app/Http/Middleware/ValidateAuthorForEditAndDeletePost.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\Post;
+use Closure;
+use Illuminate\Http\Request;
+
+class ValidateAuthorForEditAndDeletePost
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if(is_object($request->post)) {
+            if(! ($request->post->user_id == auth()->id())) {
+                return redirect(abort(401));
+            }
+        } else if(is_numeric($request->post))
+        {
+            if(! (Post::withTrashed()->findOrFail($request->post))->user_id == auth()->id()) {
+                return redirect(abort(401));
+            }
+        }
+        return $next($request);
+    }
+}
