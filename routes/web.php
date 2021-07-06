374e21bfef35dcc189b8a350edb1e791e694c529
Amaan Jambura <amaanjambura786@gmail.com>
2021-07-06 22:27:29 +0530

    customized routes for disapproving and approving post

--- routes/web.php
+++ routes/web.php
@@ -31,20 +31,23 @@ require __DIR__.'/auth.php';
 // Application routes
 // NOTE: have used raw url instead of categories.destroy
 
-Route::middleware(['auth'])->group(function(){
+Route::middleware(['auth'])->group(function () {
     Route::resource('categories', CategoriesController::class);
     Route::resource('tags', TagsController::class);
-    Route::get('posts/drafts', [PostController::class, 'drafts'])->name('posts.drafts');
-    Route::put('posts/drafts/{post}/publish', [PostController::class, 'publishDraft'])->name('posts.publish-draft');
-    Route::put('posts/restore/{post}', [PostController::class, 'restore'])->name('posts.restore');
     Route::delete('posts/trash/{post}', [PostController::class, 'trash'])->name('posts.trash');
     Route::get('posts/trashed', [PostController::class, 'trashed'])->name('posts.trashed');
+    Route::put('posts/restore/{post}', [PostController::class, 'restore'])->name('posts.restore');
+    Route::get('posts/drafts', [PostController::class, 'drafts'])->name('posts.drafts');
+    Route::get('/posts/requests', [PostController::class, 'requests'])->name('posts.approval-requests');
+    Route::put('/posts/drafts/{post}/draft', [PostController::class, 'draftPost'])->name('posts.draft-post');
+    Route::put('posts/drafts/{post}/publish', [PostController::class, 'publishDraft'])->name('posts.publish-draft');
     Route::resource('posts', PostController::class);
 });
 
 Route::middleware(['auth', 'admin'])->group(function () {
-    Route::put('/posts/approve/{post}', [PostController::class, 'approvePost'])->name('posts.approve-post');
-    Route::put('/posts/disapprove/{post}', [PostController::class, 'disapprovePost'])->name('posts.disapprove-post');
+    Route::put('posts/requests/{post}/approve', [PostController::class, 'approveRequest'])->name('posts.approve-request');
+    Route::put('posts/requests/{post}/disapprove', [PostController::class, 'disapproveRequest'])->name('posts.disapprove-request');
+    Route::put('posts/requests/{post}/reason', [PostController::class, 'disapproveReason'])->name('posts.reason');
     Route::get('/users', [UsersController::class, 'index'])->name('users.index');
     Route::put('/users/{user}/make-admin', [UsersController::class, 'makeAdmin'])->name('users.make-admin');
     Route::put('/users/{user}/revoke-admin', [UsersController::class, 'revokeAdmin'])->name('users.revoke-admin');
